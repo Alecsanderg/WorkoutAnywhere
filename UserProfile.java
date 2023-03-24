a9956c4abf57c75e0202513c1754e75268dca80b
LiamNelson9696 <liamn9696@gmail.com>
2023-03-24 11:19:15 -0700

    a

--- UserProfile.java
+++ UserProfile.java
@@ -1,8 +1,43 @@
 //contains all of the user profile data from first-time setup
 public class UserProfile
 {
-    //weight in pounds, height in inches
-    float weight, height;
+    //weight in pounds
+    private float weight;
+    //height in inches
+    private int height;
     //true = man, false = woman
-    boolean gender;
+    private boolean gender;
+
+    //needed to convert feet+inches into inches
+    int convertHeight(int feet, int inches)
+    {
+        int totalInches;
+        totalInches = inches + (12*feet);
+        return totalInches;
+    }
+
+    //Constructor for a user object
+    //w = weight, feet/inches = user's height, g = gender
+    void User(float w, int feet, int inches, boolean g)
+    {
+        //weight in pounds, height in inches
+        weight = w;
+        height = convertHeight(feet,inches);
+        gender = g;
+    }
+
+    float getWeight()
+    {
+        return weight;
+    }
+
+    int getHeight()
+    {
+        return height;
+    }
+
+    boolean getGender()
+    {
+        return gender;
+    }
 }
