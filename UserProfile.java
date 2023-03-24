5bf9d99dcbb209a701ee45cc8c71bec359b6547a
LiamNelson9696 <liamn9696@gmail.com>
2023-03-24 11:04:41 -0700

    a

---
+++ UserProfile.java
@@ -0,0 +1,8 @@
+//contains all of the user profile data from first-time setup
+public class UserProfile
+{
+    //weight in pounds, height in inches
+    float weight, height;
+    //true = man, false = woman
+    boolean gender;
+}
